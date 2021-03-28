082967ad5c657d34f9ed312860f876a97313dfc2
MEG ALAPATI <bmalapat@uwaterloo.ca>
2021-03-27 20:31:36 -0400

    load functionality corrected

--- Database/createDb.sql
+++ Database/createDb.sql
@@ -5,14 +5,78 @@ warnings;
 -- Trim Package
 select 'TrimPackage' as '';
 --create table
-create table TrimPackage (
-    -- trimPackageId int not null auto_increment,
-    trimId int,
-    trim_name varchar(125) -- primary key (trimPackageId)
-);
+create table TrimPackage (trimId int, trimName varchar(125));
 -- TODO: load csv data
-load data infile '/var/lib/mysql-files/01-Cars/used_cars_data.csv' ignore into table TrimPackage fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines (@trimId, trim_name)
-set trimId = substring(@trimId, 2);
+load data infile '/var/lib/mysql-files/01-Cars/used_cars_data.csv' ignore into table TrimPackage fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines (
+    @col1,
+    @col2,
+    @col3,
+    @col4,
+    @col5,
+    @col6,
+    @col7,
+    @col8,
+    @col9,
+    @col10,
+    @col11,
+    @col12,
+    @col13,
+    @col14,
+    @col15,
+    @col16,
+    @col17,
+    @col18,
+    @col19,
+    @col20,
+    @col21,
+    @col22,
+    @col23,
+    @col24,
+    @col25,
+    @col26,
+    @col27,
+    @col28,
+    @col29,
+    @col30,
+    @col31,
+    @col32,
+    @col33,
+    @col34,
+    @col35,
+    @col36,
+    @col37,
+    @col38,
+    @col39,
+    @col40,
+    @col41,
+    @col42,
+    @col43,
+    @col44,
+    @col45,
+    @col46,
+    @col47,
+    @col48,
+    @col49,
+    @col50,
+    @col51,
+    @col52,
+    @col53,
+    @col54,
+    @col55,
+    @col56,
+    @col57,
+    @col58,
+    @col59,
+    @col60,
+    @col61,
+    @col62,
+    @col63,
+    @col64,
+    @col65,
+    @col66
+)
+set trimId = substring(@col59, 2);
+set trimName = @col60;
 --clean table
 create temporary table dumpingGround like TrimPackage;
 insert into dumpingGround (
@@ -27,30 +91,94 @@ insert into TrimPackage (
 drop temporary table if exists dumpingGround;
 -- add PK
 alter table TrimPackage
-add primary key (trimId);
--- TODO: rename cols
+add unique (trimId, trimName);
+alter table TrimPackage
+add trimPackageId int not null auto_increment primary key first;
+
 -- Interior
 select 'Interior' as '';
 --create table
 create table Interior (
-    packageId int not null auto_increment,
-    back_legroom decimal(3, 1),
-    front_legroom decimal(3, 1),
-    interior_color varchar(125),
-    maximum_seating int
+    backLegroom decimal(3, 1),
+    frontLegroom decimal(3, 1),
+    interiorColor varchar(125),
+    maximumSeating int
 );
 -- TODO: load csv data
 load data infile '/var/lib/mysql-files/01-Cars/used_cars_data.csv' ignore into table Interior fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines (
-    @back_legroom,
-    @front_legroom,
-    interior_color,
-    @maximum_seating
+    @col1,
+    @col2,
+    @col3,
+    @col4,
+    @col5,
+    @col6,
+    @col7,
+    @col8,
+    @col9,
+    @col10,
+    @col11,
+    @col12,
+    @col13,
+    @col14,
+    @col15,
+    @col16,
+    @col17,
+    @col18,
+    @col19,
+    @col20,
+    @col21,
+    @col22,
+    @col23,
+    @col24,
+    @col25,
+    @col26,
+    @col27,
+    @col28,
+    @col29,
+    @col30,
+    @col31,
+    @col32,
+    @col33,
+    @col34,
+    @col35,
+    @col36,
+    @col37,
+    @col38,
+    @col39,
+    @col40,
+    @col41,
+    @col42,
+    @col43,
+    @col44,
+    @col45,
+    @col46,
+    @col47,
+    @col48,
+    @col49,
+    @col50,
+    @col51,
+    @col52,
+    @col53,
+    @col54,
+    @col55,
+    @col56,
+    @col57,
+    @col58,
+    @col59,
+    @col60,
+    @col61,
+    @col62,
+    @col63,
+    @col64,
+    @col65,
+    @col66
 ) -- remove trailing in
-set back_legroom = left(@back_legroom, char_length(@back_legroom) -2),
+set backLegroom = left(@col2, char_length(@col2) -2),
     -- remove trailing in
-    front_legroom = left(@front_legroom, char_length(@front_legroom) -2),
+    frontLegroom = left(@col22, char_length(@col22) -2),
+    interiorColor = @col29,
     -- remove trailing seats
-    maximum_seating = left(@front_legroom, char_length(@front_legroom) -5);
+    maximumSeating = left(@col44, char_length(@col44) -5);
 create temporary table dumpingGround like Interior;
 insert into dumpingGround (
         select distinct *
@@ -63,30 +191,95 @@ insert into Interior (
     );
 drop temporary table if exists dumpingGround;
 alter table Interior
-add primary key (
-        back_legroom,
-        front_legroom,
-        interior_color,
-        maximum_seating
+add unique (
+        backLegroom,
+        frontLegroom,
+        interiorColor,
+        maximumSeating
     );
--- TODO: rename cols
+alter table TrimPackage
+add packageId int not null auto_increment primary key first;
+
 -- WheelSystem
 select 'WheelSystem' as '';
 create table WheelSystem (
-    systemId int not null auto_increment,
     -- TODO: confirm it's 3
-    wheel_system char(3),
-    wheel_system_display varchar(40),
-    -- remove 2
+    wheelSystem char(3),
+    wheelSystemDisplay varchar(40),
     wheelbase decimal(3, 1)
 );
 -- TODO: load csv
 load data infile '/var/lib/mysql-files/01-Cars/used_cars_data.csv' ignore into table WheelSystem fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines (
-    wheel_system,
-    wheel_system_display,
-    @wheelbase
+    @col1,
+    @col2,
+    @col3,
+    @col4,
+    @col5,
+    @col6,
+    @col7,
+    @col8,
+    @col9,
+    @col10,
+    @col11,
+    @col12,
+    @col13,
+    @col14,
+    @col15,
+    @col16,
+    @col17,
+    @col18,
+    @col19,
+    @col20,
+    @col21,
+    @col22,
+    @col23,
+    @col24,
+    @col25,
+    @col26,
+    @col27,
+    @col28,
+    @col29,
+    @col30,
+    @col31,
+    @col32,
+    @col33,
+    @col34,
+    @col35,
+    @col36,
+    @col37,
+    @col38,
+    @col39,
+    @col40,
+    @col41,
+    @col42,
+    @col43,
+    @col44,
+    @col45,
+    @col46,
+    @col47,
+    @col48,
+    @col49,
+    @col50,
+    @col51,
+    @col52,
+    @col53,
+    @col54,
+    @col55,
+    @col56,
+    @col57,
+    @col58,
+    @col59,
+    @col60,
+    @col61,
+    @col62,
+    @col63,
+    @col64,
+    @col65,
+    @col66
 ) -- remove trailing in
-set wheelbase = left(@wheelbase, char_length(@wheelbase) -2);
+set wheelbase = left(@col64, char_length(@col64) -2),
+    wheelSystem = @col62,
+    wheelSystemDisplay = @col63;
 create temporary table dumpingGround like WheelSystem;
 insert into dumpingGround (
         select distinct *
@@ -99,32 +292,99 @@ insert into WheelSystem (
     );
 drop temporary table if exists dumpingGround;
 alter table WheelSystem
-add primary key (
-        wheel_system,
-        wheel_system_display,
+add unique (
+        wheelSystem,
+        wheelSystemDisplay,
         wheelbase
     );
--- TODO: rename cols
+alter table WheelSystem
+add systemId int not null auto_increment primary key first;
+
 -- Engine
 select 'Engine' as '';
 create table Engine (
-    engineId int not null auto_increment,
-    engine_cylinders varchar(40),
-    engine_displacement decimal(4, 2),
-    engine_type varchar(40),
+    engineCylinders varchar(40),
+    engineDisplacement decimal(4, 2),
+    engineType varchar(40),
     horsepower decimal(3, 2),
     transmission varchar(40),
-    transmission_display varchar(125)
+    transmissionDisplay varchar(125)
 );
 -- TODO: load csv data
 load data infile '/var/lib/mysql-files/01-Cars/used_cars_data.csv' ignore into table Engine fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines (
-    engine_cylinders,
-    engine_displacement,
-    engine_type,
-    horsepower,
-    transmission,
-    transmission_display
-);
+    @col1,
+    @col2,
+    @col3,
+    @col4,
+    @col5,
+    @col6,
+    @col7,
+    @col8,
+    @col9,
+    @col10,
+    @col11,
+    @col12,
+    @col13,
+    @col14,
+    @col15,
+    @col16,
+    @col17,
+    @col18,
+    @col19,
+    @col20,
+    @col21,
+    @col22,
+    @col23,
+    @col24,
+    @col25,
+    @col26,
+    @col27,
+    @col28,
+    @col29,
+    @col30,
+    @col31,
+    @col32,
+    @col33,
+    @col34,
+    @col35,
+    @col36,
+    @col37,
+    @col38,
+    @col39,
+    @col40,
+    @col41,
+    @col42,
+    @col43,
+    @col44,
+    @col45,
+    @col46,
+    @col47,
+    @col48,
+    @col49,
+    @col50,
+    @col51,
+    @col52,
+    @col53,
+    @col54,
+    @col55,
+    @col56,
+    @col57,
+    @col58,
+    @col59,
+    @col60,
+    @col61,
+    @col62,
+    @col63,
+    @col64,
+    @col65,
+    @col66
+)
+set engineCylinders = @col14,
+    engineDisplacement = @col15,
+    engineType = @col16,
+    horsepower = @col28,
+    transmission = @col57,
+    transmissionDisplay = @col58;
 create temporary table dumpingGround like Engine;
 insert into dumpingGround (
         select distinct *
@@ -137,33 +397,102 @@ insert into Engine (
     );
 drop temporary table if exists dumpingGround;
 alter table Engine
-add primary key (
+add unique (
         -- engine_cyclinders is not included
-        - - type->cylinders engine_displacement,
-        engine_type,
+        engineDisplacement,
+        engineType,
         horsepower,
         transmission,
-        transmission_display
+        transmissionDisplay
     );
--- TODO: rename cols
+alter table Engine
+add engineId int not null auto_increment primary key first;
+
 -- FuelSpecs
 select 'FuelSpecs' as '';
 --create table
 create table FuelSpecs (
-    specId int not null auto_increment,
-    fuel_tank_volume decimal(3, 1),
-    fuel_type varchar(40),
-    highway_fuel_economy decimal(3, 2),
-    city_fuel_economy decimal(3, 2)
+    fuelTankVolume decimal(3, 1),
+    fuelType varchar(40),
+    highwayFuelEconomy decimal(3, 2),
+    cityFuelEconomy decimal(3, 2)
 );
 -- TODO: load csv data
 load data infile '/var/lib/mysql-files/01-Cars/used_cars_data.csv' ignore into table FuelSpecs fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines (
-    @fuel_tank_volume,
-    fuel_type,
-    highway_fuel_economy,
-    city_fuel_economy
+    @col1,
+    @col2,
+    @col3,
+    @col4,
+    @col5,
+    @col6,
+    @col7,
+    @col8,
+    @col9,
+    @col10,
+    @col11,
+    @col12,
+    @col13,
+    @col14,
+    @col15,
+    @col16,
+    @col17,
+    @col18,
+    @col19,
+    @col20,
+    @col21,
+    @col22,
+    @col23,
+    @col24,
+    @col25,
+    @col26,
+    @col27,
+    @col28,
+    @col29,
+    @col30,
+    @col31,
+    @col32,
+    @col33,
+    @col34,
+    @col35,
+    @col36,
+    @col37,
+    @col38,
+    @col39,
+    @col40,
+    @col41,
+    @col42,
+    @col43,
+    @col44,
+    @col45,
+    @col46,
+    @col47,
+    @col48,
+    @col49,
+    @col50,
+    @col51,
+    @col52,
+    @col53,
+    @col54,
+    @col55,
+    @col56,
+    @col57,
+    @col58,
+    @col59,
+    @col60,
+    @col61,
+    @col62,
+    @col63,
+    @col64,
+    @col65,
+    @col66
 ) -- remove trailing gal
-set fuel_tank_volume = left(@fuel_tank_volume, char_length(@fuel_tank_volume) -3);
+set fuelTankVolume = left(
+        @col23,
+        char_length(@col23) -3
+    ),
+    fuelType = @col24,
+    highwayFuelEconomy = @col27,
+    cityFuelEconomy = @col9;
 create temporary table dumpingGround like FuelSpecs;
 insert into dumpingGround (
         select distinct *
@@ -176,19 +505,20 @@ insert into FuelSpecs (
     );
 drop temporary table if exists dumpingGround;
 alter table FuelSpecs
-add primary key (
-        fuel_tank_volume,
-        fuel_type,
-        highway_fuel_economy,
-        city_fuel_economy
+add unique (
+        fuelTankVolume,
+        fuelType,
+        highwayFuelEconomy,
+        cityFuelEconomy
     );
--- TODO: rename cols
+alter table FuelSpecs
+add specId int not null auto_increment primary key first;
+
 -- DepreciationFactors
 select 'DepreciationFactors' as '';
 create table DepreciationFactors (
-    factorId int not null auto_increment,
-    frame_damaged char(5),
-    has_accidents char(5),
-    salvage
+    frameDamaged char(5),
+    hasAccidents char(5),
+    salvage 
     savings_amount
 );
