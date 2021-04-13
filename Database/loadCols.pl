f364ebb99c47d3a4b1a287d3eca72027952ae1ea
Meg Alapati <abmfocus4@gmail.com>
2021-04-13 02:07:23 -0400

    scrip updated

--- Database/loadCols.pl
+++ Database/loadCols.pl
@@ -1,17 +1,17 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl -w
+# takes tableName and cols list as input
+$tableName = <>;
+@colNames = ("vin", "back_legroom", "bed", "bed_height", "bed_length", "body_type", "cabin", "city", "city_fuel_economy", "combine_fuel_economy", "daysonmarket", "dealer_zip", "description", "engine_cylinders", "engine_displacement", "engine_type", "exterior_color", "fleet", "frame_damaged", "franchise_dealer", "franchise_make", "front_legroom", "fuel_tank_volume", "fuel_type", "has_accidents", "height", "highway_fuel_economy", "horsepower", "interior_color", "isCab", "is_certified", "is_cpo", "is_new", "is_oemcpo", "latitude", "length", "listed_date", "listing_color", "listing_id", "longitude", "main_picture_url", "major_options", "make_name", "maximum_seating", "mileage", "model_name", "owner_count", "power", "price", "salvage", "savings_amount", "seller_rating", "sp_id", "sp_name", "theft_title", "torque", "transmission", "transmission_display", "trimId", "trim_name", "vehicle_damage_category", "wheel_system", "wheel_system_display", "wheelbase", "width", "year");  # Whatever the column names are in the table
 
-$tableName = "Cars";
-@colNames = ("vin", "back_legroom", "bed", "bed_height", "bed_length", "body_type", "cabin", "city", "city_fuel_economy", "combine_fuel_economy", "daysonmarket", "dealer_zip", "description", "engine_cylinders", "engine_displacement", "engine_type", "exterior_color", "fleet", "frame_damaged", "franchise_dealer", "franchise_make", "front_legroom", "fuel_tank_volume", "fuel_type", "has_accidents", "height", "highway_fuel_economy", "horsepower", "interior_color", "isCab", "is_certified", "is_cpo", "is_new", "is_oemcpo", "latitude", "length", "listed_date", "listing_color", "listing_id", "longitude", "main_picture_url
-", "major_options", "make_name", "maximum_seating", "mileage", "model_name", "owner_count", "power", "price", "salvage", "listing_id", "longitude");  # Whatever the column names are in the table
+$fileName = "/var/lib/mysql-files/01-Cars/used_cars_data.csv";
 
-$fileName = "/var/lib/mysql-files/somefile.csv";
-
-@cols = (4, 8, 9, 53);  # The columns I want from the CSV
+$cols = <>;
+# @cols = (4, 8, 9, 53);  # The columns I want from the CSV
 # $totalCols = 60;
 
-$terminator = "\r\n";  # Could be just "\n"
+$terminator = "\n";  # Could be just "\n"
 
-$loadCmd = "load data infile '$fileName' ignore into table $tableName fields terminated by ',' optionally enclosed by '\"' lines terminated by '$terminator' ignore 1 lines (";
+$loadCmd = "load data infile '$fileName' ignore into table $tableName fields terminated by ',' enclosed by '\"' lines terminated by '$terminator' ignore 1 lines (";
 
 $curCol = 1;
 while (scalar(@cols) > 0) {
@@ -24,6 +24,7 @@ while (scalar(@cols) > 0) {
     $nextName = shift @colNames;
     $loadCmd .= "$nextName, ";
 }
-$loadCmd .= "\@andTheRest);";
+$loadCmd .= "\@andTheRest)";
+$setCmd = <>;
 
-print $loadCmd,"\n";
+print $loadCmd,"\n", $setCmd;
